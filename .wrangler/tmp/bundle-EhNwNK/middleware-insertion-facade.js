88dba8a41e74526037037b0aa8cd9680d8bfc849
gaoyang.wu <gaoyang.wu@qq.com>
2024-10-13 19:45:03 +0800

    update

---
+++ .wrangler/tmp/bundle-EhNwNK/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "D:\\Dev\\Workspaces\\CloudFlare\\edgetunnel\\src\\_worker.js";
+				import * as __MIDDLEWARE_0__ from "D:\\Dev\\Workspaces\\CloudFlare\\edgetunnel\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "D:\\Dev\\Workspaces\\CloudFlare\\edgetunnel\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "D:\\Dev\\Workspaces\\CloudFlare\\edgetunnel\\src\\_worker.js";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
